b394a5cc56f022ff2ed42b76a8a7ee3335e84f5d
tianyi <tianyi@qianmi.com>
2019-02-01 16:12:52 +0800

    add mysql

---
+++ web/src/main/java/com/ty/PlatformApplication.java
@@ -0,0 +1,27 @@
+package com.ty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * spring-boot 启动类
+ *
+ * @author tian'yi
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ServletComponentScan
+//@MapperScan("com.com.ty.*.mapper")
+public class PlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformApplication.class, args);
+    }
+
+}
+
