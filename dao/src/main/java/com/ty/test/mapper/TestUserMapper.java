575059dcb5dc18f348d20a0c6484296de84ecab0
tianyi <tianyi@qianmi.com>
2019-02-02 14:33:52 +0800

    add gmb and test

---
+++ dao/src/main/java/com/ty/test/mapper/TestUserMapper.java
@@ -0,0 +1,14 @@
+package com.ty.test.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ty.test.model.TestUser;
+
+/**
+ * test_user
+ *
+ * @author tianyi
+ * @date 2019-02-02
+ */
+public interface TestUserMapper extends BaseMapper<TestUser> {
+
+}
---
+++ dao/src/main/java/com/ty/test/model/TestUser.java
@@ -0,0 +1,21 @@
+package com.ty.test.model;
+
+import lombok.Data;
+
+/**
+ * test_user
+ *
+ * @author tianyi
+ * @date 2019-02-02
+ */
+@Data
+public class TestUser {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String email;
+}
