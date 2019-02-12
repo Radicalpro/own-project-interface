4d7acaa3a1e8b3b375e80d51f5c8531e87395c8f
tianyi <793187547@qq.com>
2019-02-12 14:38:22 +0800

    add user model

---
+++ dao/src/main/java/com/ty/user/mapper/MyUserMapper.java
@@ -0,0 +1,14 @@
+package com.ty.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ty.user.entity.MyUserEntity;
+
+/**
+ * 用户表 Mapper 接口
+ *
+ * @author tianyi
+ * @since 2019-02-12
+ */
+public interface MyUserMapper extends BaseMapper<MyUserEntity> {
+
+}
---
+++ service/src/main/java/com/ty/user/service/IMyUserService.java
@@ -0,0 +1,16 @@
+package com.ty.user.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ty.user.entity.MyUserEntity;
+
+/**
+ * <p>
+ * 用户表 服务类
+ * </p>
+ *
+ * @author tianyi
+ * @since 2019-02-12
+ */
+public interface IMyUserService extends IService<MyUserEntity> {
+
+}
